76543a8eeeb599f00312159be4c60ccc40d33f0c
Barbarik <kukun2003@mail.ru>
2023-10-06 01:20:37 +0300

    Initial Commit

---
+++ School21/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  School21
+//
+//  Created by Andrew on 06.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
